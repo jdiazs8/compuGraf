9d0ef8fce18f97610ae32cc12f5c439497c7691a
mfmontess <mmontess@ucentral.edu.co>
2017-09-12 19:56:58 -0500

    timers y sprites

--- ApRun.py
+++ ApRun.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-#! /usr/bin env python
-# -*- coding: utf-8
-
-=======
-#!/usr/bin/python
-# -*- coding: latin-1 -*-
-# commit de prueba dos
->>>>>>> 8f4ec2e4cc3e24dd209f80173301c910cf44ef5d
 # NOMBRE DEL PROGRAMA: Apocalypsis Runner
 # AUTOR DEL PROGRAMA: Jorge Díaz, Michael Montes, Julio Morales, Johan Sánchez
 
@@ -19,13 +10,15 @@ LIENZO = [1024, 720]
 TIEMPO = 0
 DISTANCIA = 0
 PUNTAJE = 0
-image_fondo = simplegui.load_image("https://www.dropbox.com/s/thpb5tgrjfsxxvx/presentation-3-e1478644528432.png?dl=1")
+imagen_fondo = simplegui.load_image("https://www.dropbox.com/s/thpb5tgrjfsxxvx/presentation-3-e1478644528432.png?dl=1")
 obstaculos = ['https://www.dropbox.com/s/8xl28u2lqymsbio/hielo.png?dl=1',
               'https://www.dropbox.com/s/ie9osvrdytwaswy/piedra.png?dl=1',
-              'https://www.dropbox.com/s/k3ziquuae84f38t/tronco.png?dl=1']
-protagonista = ['https://www.dropbox.com/s/5kl57rh2jygfpyx/Personage.png?dl=1']
-antagonista = ['https://www.dropbox.com/s/9j08feq9aeknlgi/2017-09-11%281%29.png?dl=1']
-
+              'https://www.dropbox.com/s/k3ziquuae84f38t/tronco.png?dl=1',
+              'https://www.dropbox.com/s/4q8idgnsb9777ik/rocas7.png?dl=1']
+sprites_obstaculos = ['https://www.dropbox.com/s/7msvpmaom0p8ex2/fogataSprite.png?dl=1']
+sprites_protagonista = ['https://www.dropbox.com/s/netfiplix1wmi6b/PersonCorriendo.png?dl=1']
+antagonista = ['https://www.dropbox.com/s/gq1qjtzjolh39sa/2017-09-11.png?dl=1']
+imagen_monedas = simplegui.load_image('https://www.dropbox.com/s/m2co448t3gcpe6i/monedasOro.PNG?dl=1')
 
 # MANEJADORES DE EVENTOS
 class Escenario:
@@ -36,62 +29,6 @@ class Escenario:
 class Escenografia(Escenario):
     def __init__(self, posicion):
         self.posicion = posicion
-
-
-class Obstaculo(Escenografia):
-    def __init__(self, image, posicion):
-        self.image = image
-        self.image_tamano = [self.image.get_width(), self.image.get_height()]
-        self.posicion = posicion
-
-    def draw(self, canvas):
-        canvas.draw_image(self.image, [self.image_tamano[0] // 2, self.image_tamano[1] // 2], self.image_tamano,
-                          self.posicion, self.image_tamano)
-
-
-class Accesorio(Escenografia):
-    def __init__(self, image, posicion):
-        self.image = image
-        self.image_tamano = [self.image.get_width(), self.image.get_height()]
-        self.posicion = posicion
-
-    def draw(self, canvas):
-        canvas.draw_image(self.image, [self.image_tamano[0] // 2, self.image_tamano[1] // 2], self.image_tamano,
-                          self.posicion, self.image_tamano)
-
-
-class Personaje(Escenario):
-    def __init__(self, image, posicion):
-        self.imagePeronaje = image
-        self.image_tamano = [self.imagePeronaje.get_width(), self.imagePeronaje.get_height()]
-        self.posicion = posicion
-
-    def draw(self, canvas):
-        canvas.draw_image(self.imagePeronaje, [self.image_tamano[0] // 2, self.image_tamano[1] // 2], self.image_tamano,
-                          self.posicion, self.image_tamano)
-
-
-class Antagonista(Personaje):
-    def __init__(self, image, posicion):
-        self.imagePeronaje = image
-        self.image_tamano = [self.imagePeronaje.get_width(), self.imagePeronaje.get_height()]
-        self.posicion = posicion
-
-    def draw(self, canvas):
-        canvas.draw_image(self.imagePeronaje, [self.image_tamano[0] // 2, self.image_tamano[1] // 2], self.image_tamano,
-                          self.posicion, self.image_tamano)
-
-
-class Protagonista(Personaje):
-    def __init__(self, image, posicion):
-        self.imagePeronaje = image
-        self.image_tamano = [self.imagePeronaje.get_width(), self.imagePeronaje.get_height()]
-        self.posicion = posicion
-
-    def draw(self, canvas):
-        canvas.draw_image(self.imagePeronaje, [self.image_tamano[0] // 2, self.image_tamano[1] // 2], self.image_tamano,
-                          self.posicion, self.image_tamano)
-
 
 class Fondo(Escenografia):
     def __init__(self, image, posicion):
@@ -105,28 +42,118 @@ class Fondo(Escenografia):
         canvas.draw_text('Distancia: ' + str(DISTANCIA), (400, 50), 20, 'White', 'serif')
         canvas.draw_text('Puntos: ' + str(PUNTAJE), (800, 50), 20, 'White', 'serif')
 
+class Obstaculo(Escenografia):
+    def __init__(self, image, posicion):
+        self.image = image
+        self.image_tamano = [self.image.get_width(), self.image.get_height()]
+        self.posicion = posicion
 
+    def draw(self, canvas):
+        canvas.draw_image(self.image, [self.image_tamano[0] // 2, self.image_tamano[1] // 2], self.image_tamano,
+                          self.posicion, self.image_tamano)
+
+
+class Accesorio(Escenografia):
+    def __init__(self, image, posicion, frames, refresco):
+        self.image = image
+        self.frames = frames
+        self.refresco = refresco
+        self.time = 0
+        self.frame_centro = [0,0]
+        self.image_tamano = [self.image.get_width()//self.frames, self.image.get_height()]
+        self.image_centro = [self.image_tamano[0]//2, self.image_tamano[1]//2]
+        self.posicion = posicion
+
+    def anima_sprite(self):
+        sprite_index = (self.time% self.frames)//1
+        self.frame_centro = [self.image_centro[0] + sprite_index * self.image_tamano[0], self.image_centro[1]]
+        self.time += 1
+        
+    def draw(self, canvas):
+        canvas.draw_image(self.image, self.frame_centro, self.image_tamano, self.posicion, self.image_tamano)
+
+class Personaje(Escenario):
+    def __init__(self, image, posicion):
+        self.imagePeronaje = image
+        self.image_tamano = [self.imagePeronaje.get_width(), self.imagePeronaje.get_height()]
+        self.posicion = posicion
+
+    def draw(self, canvas):
+        canvas.draw_image(self.imagePeronaje, [self.image_tamano[0] // 2, self.image_tamano[1] // 2], self.image_tamano,
+                          self.posicion, self.image_tamano)
+
+class Protagonista(Personaje):
+    def __init__(self, image, posicion, frames, refresco):
+        self.image = image
+        self.frames = frames
+        self.refresco = refresco
+        self.time = 0
+        self.frame_centro = [0,0]
+        self.image_tamano = [self.image.get_width()//self.frames, self.image.get_height()]
+        self.image_centro = [self.image_tamano[0]//2, self.image_tamano[1]//2]
+        self.posicion = posicion
+
+    def anima_sprite(self):
+        sprite_index = (self.time% self.frames)//1
+        self.frame_centro = [self.image_centro[0] + sprite_index * self.image_tamano[0], self.image_centro[1]]
+        self.time += 1
+        
+    def draw(self, canvas):
+        canvas.draw_image(self.image, self.frame_centro, self.image_tamano, self.posicion, self.image_tamano)
+
+
+class Antagonista(Personaje):
+    def __init__(self, image, posicion, frames, refresco):
+        self.imagePeronaje = image
+        self.frames = frames
+        self.refresco = refresco
+        self.time = 0
+        self.frame_centro = [0,0]
+        self.image_tamano = [self.imagePeronaje.get_width()//self.frames, self.imagePeronaje.get_height()]
+        self.image_centro = [self.image_tamano[0]//2, self.image_tamano[1]//2]
+        self.posicion = posicion
+
+    def anima_sprite(self):
+        sprite_index = (self.time% self.frames)//1
+        self.frame_centro = [self.image_centro[0] + sprite_index * self.image_tamano[0], self.image_centro[1]]
+        self.time += 1
+        
+    def draw(self, canvas):
+        canvas.draw_image(self.imagePeronaje, self.frame_centro, self.image_tamano, self.posicion, self.image_tamano)
+    
 # MANEJADOR DE DIBUJO
 def draw_handler(canvas):
     fondo.draw(canvas)
-    obstaculo1.draw(canvas)
-    obstaculo2.draw(canvas)
-    personaje1.draw(canvas)
-    personaje2.draw(canvas)
+    rocas.draw(canvas)
+    #roca.draw(canvas)
+    protagonista.draw(canvas)
+    moneda.draw(canvas)
+    antagonista.draw(canvas)
+    fogata.draw(canvas)
 
 
 # REGISTRO DE CONTROLES Y OBJETOS
-frame = simplegui.create_frame('Sprite', LIENZO[0], LIENZO[1])
+frame = simplegui.create_frame('Apocalyse Runner', LIENZO[0], LIENZO[1])
 frame.set_draw_handler(draw_handler)
-fondo = Fondo(image_fondo, [0, 0])
-im = simplegui.load_image(obstaculos[0])
-obstaculo1 = Obstaculo(im, [600, 590])
-im = simplegui.load_image(obstaculos[1])
-obstaculo2 = Obstaculo(im, [900, 590])
-imPer = simplegui.load_image(protagonista[0])
-imPerAnt = simplegui.load_image(antagonista[0])
-personaje1 = Protagonista(imPer, [400, 590])
-personaje2 = Antagonista(imPerAnt, [90, 530])
-
+fondo = Fondo(imagen_fondo, [0, 0])
+imagen_rocas = simplegui.load_image(obstaculos[3])
+rocas = Obstaculo(imagen_rocas, [600, 590])
+#imagen_roca = simplegui.load_image(obstaculos[2])
+#roca = Obstaculo(imagen_roca, [900, 590])
+imagen_protagonista = simplegui.load_image(sprites_protagonista[0])
+imagen_antagonista = simplegui.load_image(antagonista[0])
+image_fogata = simplegui.load_image(sprites_obstaculos[0])
+fogata = Accesorio(image_fogata, [900, 480],8,100)
+protagonista = Protagonista(imagen_protagonista, [400, 590], 4, 100)
+moneda = Accesorio(imagen_monedas, [600, 450],12,70)
+antagonista = Antagonista(imagen_antagonista, [170, 530], 6, 100)
+timer_protagonista = simplegui.create_timer(protagonista.refresco, protagonista.anima_sprite)
+timer_antagonista = simplegui.create_timer(antagonista.refresco, antagonista.anima_sprite)
+timer_moneda = simplegui.create_timer(moneda.refresco, moneda.anima_sprite)
+timer_fogata = simplegui.create_timer(fogata.refresco, fogata.anima_sprite)
 # INICIO
 frame.start()
+timer_protagonista.start()
+timer_antagonista.start()
+timer_moneda.start()
+timer_fogata.start()
